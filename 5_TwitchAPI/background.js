5887fe32a7c8c8a899fb2ad270b09e9f116fbd02
Alex <alexberliner@gmail.com>
2016-03-25 12:30:28 -0400

    Finished twitch api example

--- 5_TwitchAPI/background.js
+++ 5_TwitchAPI/background.js
@@ -1,6 +1,15 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
-    Twitch.init({clientId: 'YOUR_CLIENT_ID_HERE'}, function(error, status) {
-        // the sdk is now loaded
-      });
-
+    // https://api.twitch.tv/kraken/streams/amazhs
+    var channels = ['amazhs', "nl_kripp", "reckful"];
+    for(i = 0; i < channels.length; ++i){
+        var xmlhttp = new XMLHttpRequest()
+        var url = 'https://api.twitch.tv/kraken/channels/'+channels[i];
+        xmlhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                alert(this.responseText)
+            }
+        }
+        xmlhttp.open('GET', url, true);
+        xmlhttp.send();
+    }
 });
