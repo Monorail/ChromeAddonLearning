014e8ab59a0b962041c3b9eac992e4f561406bd4
Alex <alexberliner@gmail.com>
2016-04-05 19:35:54 -0400

    Standardized promise usage

--- 10_Promises_Array/background.js
+++ 10_Promises_Array/background.js
@@ -1,20 +1,143 @@
+"use strict";
+var hsurl = "https://api.twitch.tv/kraken/channels/amazhs"
 chrome.browserAction.onClicked.addListener(function(tab) {
-    // list of streamers (and intended fails) to poll
-    var channels = ["amazhs", "nl_kripp","amsssssazhs", "nl_krisssssssssspp", "amazhs", "directory", "jobs", "amazhs"];
-    var promise_array = [];
-    //add all promises to array
-    for(var i = 0; i < channels.length; i++){
-        var promise = $.ajax({
-          url: "https://api.twitch.tv/kraken/channels/" + channels[i]
-        });
-        promise_array.push(promise);
-        //give promises individual completion conditions
-        promise.done(function(){
-            console.log("asd");
-        });
-    }
-    //give promise group completion condition
-    $.when(promise_array).then(function(){
-            console.log("all done");
-        });
-});
+        console.log("arr");
+
+    var hsurl = "https://api.twitch.tv/kraken/channels/amazhs"
+    var payload = {
+      "topic" : "js",
+      "q" : "Promise"
+    };
+    var i = 0;
+    var arr = [];
+    var callback = {
+      success : function(data){
+        console.log(1, "success", JSON.parse(data));
+        arr.push(JSON.parse(data));
+        // i = i + 1
+      },
+      error : function(data){
+        
+      }
+    };
+
+    var p1 = $http(hsurl)
+      .get(payload)
+      .then(callback.success, callback.error);
+    var p2 = $http(hsurl + "badbadbad")
+      .get(payload)
+      .then(callback.success, callback.error);
+    Promise.all([p1, p2]).then(function(values){
+        console.log("arr");
+        console.log("arr begin");
+        console.log(arr);
+        console.log("arr after");
+    });
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    function $http(url){
+      var core = {
+        ajax : function(method, url, args){
+          var promise = new Promise(function(resolve, reject){
+            var client = new XMLHttpRequest();
+            var uri = url;
+            if(args && (method === "POST" || method === "PUT")){
+              url += "?";
+              var argcount = 0;
+              for(var key in args){
+                if(args.hasOwnProperty(key)){
+                  if(argcount++){
+                    uri += "&";
+                  }
+                  uri += encodeURIComponent(key) + "=" + endcodeURLComponent(args[key]);
+                }
+              }
+            }
+            client.open(method, uri);
+            client.send();
+
+            client.onload = function(){
+              if(this.status >= 200 && this.status < 300){
+                resolve(this.response);
+              }else{
+                reject(this.statusText);
+              }
+            };
+            client.onerror = function(){
+              reject(this.statusText);
+            };
+          });
+          return promise;
+        }
+      };
+
+      return {
+        "get" : function(args){
+          return core.ajax("GET", url, args);
+        },
+        "post" : function(args){
+          return core.ajax("POST", url, args);
+        },
+        "put" : function(args){
+          return core.ajax("PUT", url, args);
+        },
+        "delete" : function(args){
+          return core.ajax("DELETE", url, args);
+        }
+      };
+    };
+    
+});//chrome  hsurl
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- 9_Promises/background.js
+++ 9_Promises/background.js
@@ -1,16 +1,119 @@
+"use strict";
+var hsurl = "https://api.twitch.tv/kraken/channels/amazhs"
 chrome.browserAction.onClicked.addListener(function(tab) {
-    console.log("Start")
-    var promise = $.ajax({
-      url: "https://api.twitch.tv/kraken/channels/amazhs"
-    });
-    promise.done(function(res){console.log("WIN")});
-    promise.fail(function(){console.log("FAIL")});
-    promise.always(function(){console.log("ALWAYS")});
+
+var payload = {
+  "topic" : "js",
+  "q" : "Promise"
+};
+
+var callback = {
+  success : function(data){
+    console.log(1, "success", JSON.parse(data));
+  },
+  error : function(data){
+  	
+  }
+};
+
+$http(hsurl)
+  .get(payload)
+  .then(callback.success, callback.error);
+$http(hsurl + "AGGGGGGGGGGGGGGGGGGGG")
+  .get(payload)
+  .then(callback.success, callback.error);
+function $http(url){
+  var core = {
+    ajax : function(method, url, args){
+      var promise = new Promise(function(resolve, reject){
+        var client = new XMLHttpRequest();
+        var uri = url;
+        if(args && (method === "POST" || method === "PUT")){
+          url += "?";
+          var argcount = 0;
+          for(var key in args){
+            if(args.hasOwnProperty(key)){
+              if(argcount++){
+                uri += "&";
+              }
+              uri += encodeURIComponent(key) + "=" + endcodeURLComponent(args[key]);
+            }
+          }
+        }
+        client.open(method, uri);
+        client.send();
+
+        client.onload = function(){
+          if(this.status >= 200 && this.status < 300){
+            resolve(this.response);
+          }else{
+            reject(this.statusText);
+          }
+        };
+        client.onerror = function(){
+          reject(this.statusText);
+        };
+      });
+      return promise;
+    }
+  };
+
+  return {
+    "get" : function(args){
+      return core.ajax("GET", url, args);
+    },
+    "post" : function(args){
+      return core.ajax("POST", url, args);
+    },
+    "put" : function(args){
+      return core.ajax("PUT", url, args);
+    },
+    "delete" : function(args){
+      return core.ajax("DELETE", url, args);
+    }
+  };
+};
     
-    promise = $.ajax({
-      url: "https://api.twitch.tv/kraken/channels/amazhsasdfgbasuhdijfk"
-    });
-    promise.done(function(res){console.log("WIN")});
-    promise.fail(function(){console.log("FAIL")});
-    promise.always(function(){console.log("ALWAYS")});
-});
+});//chrome  hsurl
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
